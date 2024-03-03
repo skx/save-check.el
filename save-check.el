66bf34a30d5534f0acf8f43f2028d71f4acb371e
Steve Kemp <steve@steve.org.uk>
2024-03-03 06:26:16 +0200

    Added header, and more documentation

--- save-check.el
+++ save-check.el
@@ -1,13 +1,61 @@
 ;;; save-check.el - Perform syntax checks AFTER saving files
 
+;;
+;; Copyright (C) 2024 Steve Kemp
+;;
+;; Version: 0.1
+;; Keywords: perl, dockerfile, json, python, shell, terraform, linter, xml, yaml
+;; Author: Steve Kemp <steve@steve.fi>
+;;
+
+;; This file is not (YET) part of GNU Emacs.
+
+;; This is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 2, or (at your option) any later
+;; version.
+;;
+;; This is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;; for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+;; MA 02111-1307, USA.
+
+;;;
+;;
+
+;;
+;; This package defines a standard way of running linters, automatically,
+;; when files are saved.
+;;
+;; The intention is that we can add a global hook to be executed when files
+;; are saved, and within that hook "do the right thing" - which means run the
+;; appropriate linter based on the buffers's major-mode.
+;;
+;; Several sane linters are defined within this package, and adding new ones
+;; is straightforward.
+;;
 
 
-;; The default linters that we ship with.
+
+
+
+;; The default linters that we are configured to run.
+;;
+;; Within the commands limiting expansion takes place:
+;;
+;;     %f is replaced with the filename to lint.
+;;     %d is replaced with the directory containing the file.
+;;
 (defvar save-check-config
       '(
 
         (:mode cperl-mode
-         :exec "perl -wc -I. %s"
+         :exec "perl -wc -I. %f"
          :cond (executable-find "perl"))
 
         (:mode dockerfile-mode
@@ -55,15 +103,12 @@ lisp expression specified in `:eval' will be evaluated.")
   nil
   "If this variable is set then we show the output of lisp expressions evaluated with the `:eval' key.
 
-Otherwise they are discarded.")
+Otherwise any output, or return value, is discarded.")
 
 
 (defvar save-check-buffer-name
   "*save-check*"
   "The name of the buffer which is used to show failing results, if any.")
-
-
-
 
 
 (defun save-check-config-ok-p ()
@@ -99,11 +144,14 @@ If either sanity-check fails then an error is raised."
 If the current `major-mode' matches an entry in `save-check-config' then
 a check will be carried out.
 
-   A check is typically called by executing an external process, and testing
+A check is typically called by executing an external process, and testing
 the status code of that call, however it is also possible to call an arbitrary
 lisp expression.
 
-  If a command is listed in the `:exec' key it will be invoked via `save-check-run-command',
+If an external command is executed and terminates with a non-zero exit code
+its output will be displayed in a buffer for reference.
+
+If a command is listed in the `:exec' key it will be invoked via `save-check-run-command',
 if a lisp expression is given under the `:eval' key it will be invoked by `save-check-run-lisp'."
   (interactive)
 
